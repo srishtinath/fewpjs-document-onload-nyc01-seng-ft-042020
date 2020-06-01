a48f45e8c62198d1d2ff97cc877ec1ee00f41c1d
Srishti Nath <srishti.nath@gmail.com>
2020-06-01 18:02:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let text=document.getElementById('text');
+    text.textContent = "This is really cool!";
+    
+  });
+
+//   console.log("Maybe the internet is slow");
